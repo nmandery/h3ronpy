afbdeaeffbc8ff6bb14205a98ce9157997d933ba
Kyle Barron <kyle@developmentseed.org>
2024-10-15 13:11:58 -0400

    start fixing tests

--- h3ronpy/tests/polars/test_utf8.py
+++ h3ronpy/tests/polars/test_utf8.py
@@ -4,6 +4,8 @@ from h3ronpy.polars import cells_parse, cells_valid, cells_to_string
 import numpy as np
 import h3.api.numpy_int as h3
 import polars as pl
+from arro3.core import DataType
+import pyarrow as pa
 
 
 def test_cells_parse():
@@ -37,7 +39,7 @@ def test_parse_cell_set_invalid():
     )
     cells = cells_parse(strings, set_failing_to_invalid=True)
     assert len(cells) == 1
-    assert cells[0] is None
+    assert not cells[0].is_valid
 
 
 def test_cells_valid():
@@ -47,17 +49,17 @@ def test_cells_valid():
     )
     cells = cells_valid(input, booleanarray=False)
     assert len(cells) == 2
-    assert cells.dtype == pl.datatypes.UInt64()
-    assert cells[0] is None
-    assert cells[1] is not None
+    assert cells.type == pa.uint64()
+    assert not cells[0].is_valid
+    assert cells[1].is_valid
 
     bools = cells_valid(input, booleanarray=True)
     assert len(bools) == 2
-    assert bools.dtype == pl.datatypes.Boolean()
-    assert bools[0] is False
-    assert bools[1] is True
+    assert bools.type == pa.bool_()
+    assert bools[0].as_py() is False
+    assert bools[1].as_py() is True
 
-    assert cells.is_not_null().eq(bools).all()
+    assert pa.array(cells).is_valid() == pa.array(bools)
 
 
 def test_cells_to_string():
