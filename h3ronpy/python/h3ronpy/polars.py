b713e4f319d2e763bcd301e1dee09d403afa78e8
Justus Magin <keewis@posteo.de>
2026-01-06 12:39:21 +0100

    more return dtypes for polars

--- h3ronpy/python/h3ronpy/polars.py
+++ h3ronpy/python/h3ronpy/polars.py
@@ -70,7 +70,8 @@ class H3Expr:
 
     def cells_parse(self, set_failing_to_invalid: bool = False) -> pl.Expr:
         return self.__expr_map_series(
-            lambda s: h3ronpy.cells_parse(s, set_failing_to_invalid=set_failing_to_invalid)
+            lambda s: h3ronpy.cells_parse(s, set_failing_to_invalid=set_failing_to_invalid),
+            return_dtype=pl.UInt64,
         ).alias("cell")
 
     def vertexes_parse(self, set_failing_to_invalid: bool = False) -> pl.Expr:
@@ -80,7 +81,8 @@ class H3Expr:
 
     def directededges_parse(self, set_failing_to_invalid: bool = False) -> pl.Expr:
         return self.__expr_map_series(
-            lambda s: h3ronpy.directededges_parse(s, set_failing_to_invalid=set_failing_to_invalid)
+            lambda s: h3ronpy.directededges_parse(s, set_failing_to_invalid=set_failing_to_invalid),
+            return_dtype=pl.UInt64,
         ).alias("directededge")
 
     def grid_disk(self, k: int, flatten: bool = False) -> pl.Expr:
@@ -89,37 +91,37 @@ class H3Expr:
         )
 
     def compact(self, mixed_resolutions: bool = False) -> pl.Expr:
-        return self.__expr_map_series(lambda s: h3ronpy.compact(s, mixed_resolutions=mixed_resolutions))
+        return self.__expr_map_series(lambda s: h3ronpy.compact(s, mixed_resolutions=mixed_resolutions), return_dtype=pl.UInt64)
 
     def uncompact(self, target_resolution: int) -> pl.Expr:
-        return self.__expr_map_series(lambda s: h3ronpy.uncompact(s, target_resolution))
+        return self.__expr_map_series(lambda s: h3ronpy.uncompact(s, target_resolution), return_dtype=pl.UInt64)
 
     def cells_area_m2(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.cells_area_m2).alias("area_m2")
+        return self.__expr_map_series(h3ronpy.cells_area_m2, return_dtype=pl.Float64).alias("area_m2")
 
     def cells_area_km2(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.cells_area_km2).alias("area_km2")
+        return self.__expr_map_series(h3ronpy.cells_area_km2, return_dtype=pl.Float64).alias("area_km2")
 
     def cells_area_rads2(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.cells_area_rads2).alias("area_rads2")
+        return self.__expr_map_series(h3ronpy.cells_area_rads2, return_dtype=pl.Float64).alias("area_rads2")
 
     def cells_valid(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.cells_valid).alias("cells_valid")
+        return self.__expr_map_series(h3ronpy.cells_valid, return_dtype=pl.Boolean).alias("cells_valid")
 
     def vertexes_valid(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.vertexes_valid).alias("vertexes_valid")
+        return self.__expr_map_series(h3ronpy.vertexes_valid, return_dtype=pl.Boolean).alias("vertexes_valid")
 
     def directededges_valid(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.directededges_valid).alias("directededges_valid")
+        return self.__expr_map_series(h3ronpy.directededges_valid, return_dtype=pl.Boolean).alias("directededges_valid")
 
     def cells_to_string(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.cells_to_string)
+        return self.__expr_map_series(h3ronpy.cells_to_string, return_dtype=pl.String)
 
     def vertexes_to_string(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.vertexes_to_string)
+        return self.__expr_map_series(h3ronpy.vertexes_to_string, return_dtype=pl.String)
 
     def directededges_to_string(self) -> pl.Expr:
-        return self.__expr_map_series(h3ronpy.directededges_to_string)
+        return self.__expr_map_series(h3ronpy.directededges_to_string, return_dtype=pl.String)
 
 
 @pl.api.register_series_namespace("h3")
